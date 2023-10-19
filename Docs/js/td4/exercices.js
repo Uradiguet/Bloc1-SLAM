54969bc8239b4f06cf0818e752d66d7edf0f34bc
Ugo Radiguet <ugo.radiguet@sts-sio-caen.info>
2023-10-19 12:56:20 +0200

    td4 finito

--- Docs/js/td4/exercices.js
+++ Docs/js/td4/exercices.js
@@ -6,22 +6,25 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-const $=function(id){
-    return document.getElementById(id)
-}
-/*
-const bt = $("bt-bg");
-bt.addEventListener("click", function f() {
+const $=(id)=>document.getElementById(id);
+
+const addEvt=function (type,id,listener){
+    const elm=$(id);
+    elm && elm.addEventListener(type,listener);
+};
+
+addEvt("click","bt-bg", function() {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     const randomFontColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
     document.body.style.color = "#" + randomFontColor;
 });
 
-document.getElementById("bt-bg2").addEventListener("click", function(){
-    const color = document.getElementById("select-color").value;
+addEvt("click","bt-bg2", function(){
+    const color = document.$("select-color").value;
     document.body.style.backgroundColor=color;
-})*/
+})
+
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -29,16 +32,15 @@ document.getElementById("bt-bg2").addEventListener("click", function(){
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-const add = $("bt-add");
-add.addEventListener("click", function(){
-    document.querySelectorAll(".list").forEach(
-        (elm)=>{
-            const li = document.createElement(elm.tagName=="SELECT"?"option":"li");
-            li.innerText=$("select-text").value;
-            elm.appendChild(li);
-        }
+addEvt("click","bt-add",function(){
+    document.querySelectorAll(".liste").forEach(
+    (elm)=>{
+        const tagName=elm.tagName=="Select"?"option":"li";
+    const li=document.createElement("li");
+    li.innerText=$("selected-text").value;
+    elm.appendChild(li);
+    }  
     );
-    
 });
 
 
@@ -48,32 +50,109 @@ add.addEventListener("click", function(){
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-/*
 
+const addition=()=>{
+    const nombre1=parseFloat($("number1").value);
+    const nombre2=parseFloat($("number2").value);
+    const resultat=nombre1+nombre2;
+    $("resultat").textContent=resultat;
+};
+addEvt("click", "calcul", addition);
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//EX4
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+const SelectImage=()=>{
+    const ImageSelector=$("imageSelector");
+    const DisplayedImage=$("displayedImage");
 
+    const selectedValue=ImageSelector.value;
+    DisplayedImage.src=selectedValue;
+};
+addEvt("change", "imageSelector", SelectImage);
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//EX5
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+const afficherAlerte=()=>{
+    alert("0100 1011 1000 1010 0001 0010 0110 1111 1110 0000 1100 0011");
+    alert("Vous étiez pourtant prévenu !");
+};
+addEvt("mouseover", "titre", afficherAlerte);
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//EX6
 
-$("bt-clear").addEventListener("click", function(){
-    document.body.innerHTML="";
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function Horloge(){
+    const horlogeElement=$('horloge');
+    const maintenant=new Date();
+    const heures=maintenant.getHours().toString().padStart(2, '0');
+    const minutes=maintenant.getMinutes().toString().padStart(2, '0');
+    const secondes=maintenant.getSeconds().toString().padStart(2, '0');
+    const heureActuelle=`${heures}:${minutes}:${secondes}`;
+    horlogeElement.textContent=heureActuelle;
+}
+setInterval(Horloge, 1000);
+Horloge();
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//EX7
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+addEvt('click', 'boutonSupprimer', function () {
+    const confirmation=confirm("Voulez-vous supprimer cet élément ?");
+    if (confirmation){
+        const elementASupprimer = $('boutonSupprimer');
+        elementASupprimer.parentNode.removeChild(elementASupprimer);
+        alert("L'élément a été supprimé.");
+    } else{
+        alert("L'élément n'a pas été supprimé.");
+    }
 });
 
-$("bt-dom").addEventListener("click", function(){
-    const h2=document.createElement("h2");
-    h2.innerText=("SOUS TITRE AJOUTÉ DYNAMIQUEMENT");
-    $("content").appendChild(h2);
-    const img = document.createElement("img");
-    img.src="https://static.wikia.nocookie.net/joueur-du-grenier/images/4/43/David_Good.jpg/revision/latest/scale-to-width-down/1200?cb=20200629174229&path-prefix=fr"
-    img.style.width="200px";
-    $("content").appendChild(img);
-});
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-*/
+//EX8
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const cacherOuAfficherTexte=()=>{
+    const checkbox=$("checkbox");
+    const texte=$("texte");
+    if(checkbox.checked){
+        texte.style.display = "block";
+    } else{
+        texte.style.display = "none";
+    }
+};
+addEvt("change", "checkbox", cacherOuAfficherTexte);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//EX9
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const envoyerFormulaire=(e)=>{
+    e.preventDefault();
+    const nom = $("nom").value;
+    const email = $("email").value;
+    const message = $("message").value;
+    console.log("Nom :", nom);
+    console.log("E-mail :", email);
+    console.log("Message :", message);
+};
+addEvt("submit", "contactForm", envoyerFormulaire);
+
+

3587cbe7a05bd5bd3f028bca9bb4fccc9cadaedb
Hugo Radiguet <uradiguet@sio.local>
2023-11-16 17:08:12 +0100

    ex4 non fini

--- Docs/pages/td5/Lorem-ipsum-gen.php
+++ Docs/pages/td5/Lorem-ipsum-gen.php
@@ -1,15 +1,6 @@
 <?php
-
-    $fonts=['serif','sans-serif','cursive','monospace','verdana'];
-
-    extract($_POST);
-    $message ??='Hello World !';
-    $color??='black';
-    $size??='15';
-    $italic=isset($ckIta)?";font-style: italic":"";
-    $italicCk=$italic?"checked":"";
-    $bold=isset($ckBold)?";font-weight: bold":"";
-    $boldCk = $bold?"checked":"";
+    $array??=[];
+    $number??='15';
 ?>
 
 <!DOCTYPE html>
@@ -27,60 +18,12 @@
 </style>
 <body>
 
-    <p style="color:<?=$color ?>; font-size:<?= $size?>px; <?=$italic?>; <?=$bold?>">
-        <?=$message?>
-    </p>
+    <div>
+        <label for="number">Nombre de paragraphes :</label>
+        <input type="number" id="number" name="number" value=<?=$number?>min="1" max="30">
+    </div>
 
-
-
-    <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
-        <div>
-            <label for="message">Message :</label>
-            <textarea id="message" name="message" placeholder="Tapez votre message"><?=$message?>
-            </textarea>
-        </div>
-        <div>
-            <label for="color">Couleur :</label>
-            <input type="color" id="color" name="color" value=<?=$color?>>
-        </div>
-        <div>
-            <label for="size">taille :</label>
-            <input type="number" id="size" name="size" value=<?=$size?>min="10" max="150">
-        </div>
-
-        <div>
-            <input type="checkbox" id="ckBold" name="ckBold" value="bold" <?=$boldCk?>>
-            <label for="ckBold">Gras</label>
-            <input type="checkbox" id="ckIta" name="ckIta" value="italic" <?=$italicCk?>>
-            <label for="ckIta">Italique</label>
-        </div>
-
-        <div>
-            <label for='font'>Famille de police</label>
-            <select name="font" id="font">
-            <?php
-                foreach($fonts as $font){
-                    echo "<option value='$font'>$font</option>";
-                }
-                ?>
-                </select>
-
-        </div>
-
-        <button type="submit">Valider</button>
-
-    </form>
-
-
-
-
-    <h2> URL de test</h2>
-
-    <ul>
-        <li><a href="?message=TEXTE EN VERT DE TAILLE 30&size=30&color=green">Vert</a></li>
-        <li><a href="?message=TEXTE EN GRIS DE TAILLE 20&size=20&color=grey">Gris</a></li>
-        <li><a href="?message=TEXTE EN BLEU DE TAILLE 40&size=40&color=blue">Bleu</a></li>
-    </ul>
-
+    <button type="submit">Valider</button>
+    
 </body>
 </html>
